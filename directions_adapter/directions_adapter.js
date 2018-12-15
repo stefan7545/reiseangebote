5d541cb2a1a5a69bd166a5c2fbdd917d79091235
Stefan Eibl <stefan.eibl@student.unisg.ch>
2018-12-15 21:20:21 +0100

    test developers api

---
+++ directions_adapter/directions_adapter.js
@@ -0,0 +1,16 @@
+const API_KEY = 'AIzaSyDwMehy8bb37T65BetCsgavnh3FCXFI0N8';
+
+function request_direction() {
+    var params = {
+        'origin': "7+Jahnstraße+Holzkirchen+BY",
+        'destination': "201+Mainzer+Landstraße+Frankfurt+am+Main+HE",
+        'key': API_KEY
+    };
+    console.log(params);
+    $.ajax({
+        url: "http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyAYKsG8djFd7WfSXzgG5_BVQLD8bzd-JFE"
+    })
+        .done(function (result) {
+            console.log(result);
+        })
+}
